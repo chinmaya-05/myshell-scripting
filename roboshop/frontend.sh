2cc40f7a7560a6530b4d2cb322e5157d7625a7e5
chinmaya-05 <chinmayahota5@gmail.com>
2023-07-05 23:01:58 +0530

    Auto push

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+COMPONENT=frontend
 ID=$(id -u)
 if [$ID ne 0]; then
     echo -e "\e[31m THis script is expted to be run by root user or by sudo \e [0m]"
@@ -6,5 +7,5 @@ if [$ID ne 0]; then
 
 fi
 echo "Installaing Nginx"
-yum install nginx -y 
+yum install nginx -y &>> "/tmp/${COMPONENT}.log"
 #if script is executed or as a sudo user then it ha sto proceed , if not I want to exit teh script with soem message
