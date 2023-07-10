4dfcb99568978d12df7991f6e733010f36cb0241
chinmaya-05 <chinmayahota5@gmail.com>
2023-07-10 12:02:33 +0530

    Auto push

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 COMPONENT=frontend
 ID=$(id -u)
-if [ $ID ne 0 ]; then
+if [ $ID -ne 0 ]; then
     echo -e "\e[31m THis script is expted to be run by root user or by sudo \e [0m]"
     exit 1
 
 fi
+stats(){
+if [ $1 -eq 0 ] ; then 
+   echo "Success"
+else 
+   echo "Failed"
+   exit 2
+fi
+}
 echo "Installaing Nginx"
 
 yum install nginx -y &>> "/tmp/${COMPONENT}.log"
 
-if [ $? -eq 0 ] ; then 
-   echo "Success"
-else 
-   echo "Failed"
-fi
+stats $1?
 
-echo -n "Down;oadiong the front end componenet "
+echo -n "Downloadiong the front end componenet "
 curl -s -L -o /tmp/frontend.zip "# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip""
 #if script is executed or as a sudo user then it ha sto proceed , if not I want to exit teh script with soem message
+
+stats $1?
+
+
+echo -n "Performing cleaning up"
+cd /usr/share/nginx/html
+rm -rf * &>> "/tmp/${COMPONENT}.log"
+stats $1?
